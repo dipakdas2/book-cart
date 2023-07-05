97083ded2c9a45654dd68c6b68aa8d0e303574e8
dipakdas2 <dipakdas2@gmail.com>
2023-07-05 22:06:56 +0530

    Test cases updated for various pack of book with discounts

--- BookCartTest/src/test/java/com/book/cart/BookCartTest.java
+++ BookCartTest/src/test/java/com/book/cart/BookCartTest.java
@@ -13,13 +13,98 @@ public class BookCartTest {
 	@Test
 	public void test_firstCopy_price(){
 		
+		BookCart bookCart = new BookCart();
 		List<String> books = new ArrayList<String>();
 		books.add(BookUtil.BOOK_ONE);
 		
-		double amount = BookCart.calculatePrice(books);
+		double amount = bookCart.calculateCartPrice(books);
 		
 		Assert.assertThat(50.0, is(amount));
 		
 	}
-
+	
+	@Test
+	public void test_firstTwoCopy_price(){
+		
+		BookCart bookCart = new BookCart();
+		List<String> books = new ArrayList<String>();
+		books.add(BookUtil.BOOK_ONE);
+		books.add(BookUtil.BOOK_TWO);
+		
+		double amount = bookCart.calculateCartPrice(books);
+		
+		Assert.assertThat(95.0, is(amount));
+		
+	}
+	
+	@Test
+	public void test_firstThreeCopy_price(){
+		
+		BookCart bookCart = new BookCart();
+		List<String> books = new ArrayList<String>();
+		books.add(BookUtil.BOOK_ONE);
+		books.add(BookUtil.BOOK_TWO);
+		books.add(BookUtil.BOOK_THREE);
+		
+		double amount = bookCart.calculateCartPrice(books);
+		
+		Assert.assertThat(135.0, is(amount));
+		
+	}
+	
+	@Test
+	public void test_firstFourCopy_price(){
+		
+		BookCart bookCart = new BookCart();
+		List<String> books = new ArrayList<String>();
+		books.add(BookUtil.BOOK_ONE);
+		books.add(BookUtil.BOOK_TWO);
+		books.add(BookUtil.BOOK_THREE);
+		books.add(BookUtil.BOOK_FOUR);
+		
+		double amount = bookCart.calculateCartPrice(books);
+		
+		Assert.assertThat(160.0, is(amount));
+		
+	}
+	
+	
+	@Test
+	public void test_firstFiveCopy_price(){
+		
+		BookCart bookCart = new BookCart();
+		List<String> books = new ArrayList<String>();
+		books.add(BookUtil.BOOK_ONE);
+		books.add(BookUtil.BOOK_TWO);
+		books.add(BookUtil.BOOK_THREE);
+		books.add(BookUtil.BOOK_FOUR);
+		books.add(BookUtil.BOOK_FIVE);
+		
+		double amount = bookCart.calculateCartPrice(books);
+		
+		Assert.assertThat(187.5, is(amount));
+		
+	}
+	
+	@Test
+	public void test_MultiCopy_price(){
+		
+		BookCart bookCart = new BookCart();
+		List<String> books = new ArrayList<String>();
+		books.add(BookUtil.BOOK_ONE);
+		books.add(BookUtil.BOOK_TWO);
+		books.add(BookUtil.BOOK_THREE);
+		
+		books.add(BookUtil.BOOK_ONE);
+		books.add(BookUtil.BOOK_TWO);
+		books.add(BookUtil.BOOK_THREE);
+		
+		books.add(BookUtil.BOOK_FOUR);
+		books.add(BookUtil.BOOK_FIVE);
+		
+		double amount = bookCart.calculateCartPrice(books);
+		
+		Assert.assertThat(320.0, is(amount));
+		
+	}
 }
